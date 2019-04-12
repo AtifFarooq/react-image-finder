1b3348911101d75ad6e380e56ab8390a8506a391
Atif Farooq <u6104602@anu.edu.au>
2019-04-12 19:04:08 +1000

    Updates

---
+++ precache-manifest.434199a7b414c784c67ef23b8e08495c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b6035a280f7005cdbe1",
+    "url": "/react-image-finder/static/js/runtime~main.003b8d01.js"
+  },
+  {
+    "revision": "f35b7a65ac838f1fd19a",
+    "url": "/react-image-finder/static/js/main.add3a187.chunk.js"
+  },
+  {
+    "revision": "a24d9316fbe8c02d4f16",
+    "url": "/react-image-finder/static/js/2.ea576548.chunk.js"
+  },
+  {
+    "revision": "f35b7a65ac838f1fd19a",
+    "url": "/react-image-finder/static/css/main.65c59df3.chunk.css"
+  },
+  {
+    "revision": "81fe1c47918804e1d98ca569539278bf",
+    "url": "/react-image-finder/index.html"
+  }
+];
